3621357fae58c9a44fcef2bfb20f1dba218d7df6
hoony8355 <84762407+hoony8355@users.noreply.github.com>
2025-06-09 04:22:14 +0900

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,10 +1,13 @@
 // firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-analytics.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-database.js";
 
+// Firebase 설정
 const firebaseConfig = {
   apiKey: "AIzaSyCbgziR_rX4O9OkDBsJxTzNO3q486C_eH4",
   authDomain: "sudokudo-58475.firebaseapp.com",
+  databaseURL: "https://sudokudo-58475-default-rtdb.asia-southeast1.firebasedatabase.app",  // 리전 반영된 정확한 주소
   projectId: "sudokudo-58475",
   storageBucket: "sudokudo-58475.firebasestorage.app",
   messagingSenderId: "759625494323",
@@ -12,7 +15,10 @@ const firebaseConfig = {
   measurementId: "G-5YCQ6KGK43"
 };
 
+// Firebase 초기화
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getDatabase(app);
 
-export { app, db };
+// 필요한 것들 export
+export { db, analytics };
